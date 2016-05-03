b82c0143262f7418a799f3b711acdb7eda40f40e
Jacin Montava <jacinto.manuel.montava.navarro@everis.com>
2016-05-03 08:58:15 +0200

    Velocimetro coursera finished

--- VelocimetroCoursera.playground/Contents.swift
+++ VelocimetroCoursera.playground/Contents.swift
@@ -2,4 +2,52 @@
 
 import UIKit
 
-var str = "Hello, playground"
+enum Velocidades: Int {
+    case Apagado = 0,
+    VelocidadBaja = 20,
+    VelocidadMedia = 50,
+    VelocidadAlta = 120
+    
+    init(velocidadInicial: Velocidades){
+        self = velocidadInicial
+    }
+}
+
+class Auto {
+    var velocidad: Velocidades
+    var velocidadString: String
+    
+    init () {
+        self.velocidad = Velocidades.Apagado
+        self.velocidadString = "Apagado"
+    }
+    
+    func cambioDeVelocidad(velocidad: Velocidades) -> (actual: Int, velocidadesEnCadena: String){
+        
+        switch velocidad.hashValue {
+        case 0:
+            print("\(self.velocidad.rawValue), \(self.velocidadString)")
+            self.velocidad = .VelocidadBaja
+            self.velocidadString = "Velocidad Baja"
+        case 1:
+            self.velocidad = .VelocidadMedia
+            self.velocidadString = "Velocidad Media"
+        case 2:
+            self.velocidad = .VelocidadAlta
+            self.velocidadString = "Velocidad Alta"
+        case 3:
+            self.velocidad = .VelocidadMedia
+            self.velocidadString = "Velocidad Media"
+            
+        default:
+            self.velocidad = velocidad
+        }
+        return (self.velocidad.rawValue, self.velocidadString)
+    }
+}
+
+var auto: Auto = Auto()
+for _ in 1...20 {
+    var tupla = auto.cambioDeVelocidad(auto.velocidad)
+    print("\(tupla.actual), \(tupla.velocidadesEnCadena)")
+}
